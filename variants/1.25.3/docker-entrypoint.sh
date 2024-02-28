826a8ce0b4672597be63b22a7a7fe8fdc1856947
Andrea Patricelli <andreapatricelli@apache.org>
2024-02-28 09:08:49 +0100

    fixes #8

--- variants/1.25.3/docker-entrypoint.sh
+++ variants/1.25.3/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 RESOLVER=${RESOLVER:-}
 if [ -n "$RESOLVER" ]; then
     echo "Applying: resolver $RESOLVER;"
-    sed -i "s/# resolver.*/resolver $RESOLVER;/" /usr/local/nginx/conf/nginx.conf
+    sed -i "s/resolver.*/resolver $RESOLVER;/" /usr/local/nginx/conf/nginx.conf
 fi
 
 if [ $# -gt 0 ] && [ "${1#-}" != "$1" ]; then
